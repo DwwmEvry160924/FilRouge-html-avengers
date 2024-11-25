31ad3c036672ac1fd543a51ecec91d247eb5b1b2
edusilvaramos <edusilvaramos@hotmail.com>
2024-11-25 18:25:54 +0100

    [ER:refactor] add js functionality

--- assets/js/newProject.js
+++ assets/js/newProject.js
@@ -6,10 +6,16 @@ getTodayDate();
 const date_Fin = document.getElementById("Date-fin");
 const Description = document.getElementById("Description");
 const chef_pro = document.getElementById("Chef");
+const image = document.getElementById("img");
+
+let list_json = []; // list of projects
+
+list_json = JSON.parse(localStorage.getItem("projects")) || [];
+console.log(list_json);
 
 btn_creer.addEventListener("click", (e) => {
   if (
-    input_nom.value.trim() === "" ||
+    input_nom.value.trim() === "" || //ps: The trim() method removes whitespace from both sides of a string
     date_Fin.value.trim() === "" ||
     Description.value.trim() === "" ||
     chef_pro.value === "choisissez le Chef de projet"
@@ -30,9 +36,36 @@ btn_creer.addEventListener("click", (e) => {
         console.log(`Membres : ${membre.value}`);
       }
     });
+    console.log(`image : ${image.value}`);
     console.log("---------------------");
     alert(`Le projet ${input_nom.value} a bien ete cree`);
-    window.location.href = "projet.html";
+    
+
+    // ----------------- put in the local storage ------------------//
+
+    let list_membres = []; // list of membres for to put in the database
+
+    membres.forEach((membre) => {
+      // to get the membres has checked
+      if (membre.checked) {
+        list_membres.push(membre.value);
+      }
+    });
+
+    list_json.push({
+      id: list_json.length + 1,
+      nom: input_nom.value,
+      image: image.value, // to get the image do pro
+      "date de fin": date_Fin.value,
+      description: Description.value,
+      chef: chef_pro.value,
+      membres: list_membres,
+    });
+    console.log("---------------------");
+
+    localStorage.setItem("projects", JSON.stringify(list_json)); // to save the projects in the local storage
+    console.log(localStorage.getItem("projects"));
+    window.location.href = "index.html";
   }
 });
 function validateForm() {
@@ -48,8 +81,8 @@ function validateForm() {
 // validation nom
 function validation_nom() {
   const valid_input = document.getElementById("valid-Non");
-  input_nom.value.trim() === ""
-    ? (valid_input.style.display = "block")
+  input_nom.value.trim() === "" // remove whitespace
+    ? (valid_input.style.display = "block") // manipulation display style
     : (valid_input.style.display = "none");
 }
 
@@ -58,8 +91,8 @@ function validation_nom() {
 function validation_date() {
   const valid_date = document.getElementById("valid-Date");
 
-  if (date_Fin.value.trim() === "" || date_Fin.value < today) {
-    valid_date.style.display = "block";
+  if (date_Fin.value.trim() === "") {
+    valid_date.style.display = "block"; // manipulation display style
   } else {
     valid_date.style.display = "none";
   }
@@ -67,7 +100,7 @@ function validation_date() {
 // to get today date
 function getTodayDate() {
   const date_Init = document.getElementById("Date-init");
-  let today = new Date();
+  let today = new Date(); // get the current date
   let day = String(today.getDate()).padStart(2, "0");
   let month = String(today.getMonth() + 1).padStart(2, "0");
   let year = today.getFullYear();
@@ -76,22 +109,24 @@ function getTodayDate() {
 // validation description
 function validation_description() {
   const valid_description = document.getElementById("valid-Description");
-  Description.value.trim() === ""
+  Description.value.trim() === "" // remove whitespace
     ? (valid_description.style.display = "block")
-    : (valid_description.style.display = "none");
+    : (valid_description.style.display = "none"); // manipulation display style
 }
 // validation membres
 function get_membres() {
   const membres = document.querySelectorAll("input[type=checkbox]"); //get all checkbox of membres
   let has_checked = false;
   membres.forEach((membre) => {
+    // to check if at least one checkbox is checked
     if (membre.checked) {
       has_checked = true;
     }
   });
-  const valid_membre = document.getElementById("valid-membre");
+  const valid_membre = document.getElementById("valid-membre"); // look for the element
   if (has_checked) {
-    valid_membre.style.display = "none";
+    // get the membres checked
+    valid_membre.style.display = "none"; // manipulation display style of membres checked
   } else {
     valid_membre.style.display = "block";
   }
@@ -99,18 +134,17 @@ function get_membres() {
 
 // validation chef_pro
 function validation_chef_pro(choice) {
-  const valid_description = document.getElementById("valid-Chef");
+  const valid_description = document.getElementById("valid-Chef"); // to get input chef du projet
   choice = choice.value;
   choice === "choisissez le Chef de projet"
-    ? (valid_description.style.display = "block")
+    ? (valid_description.style.display = "block") // manipulation display style
     : (valid_description.style.display = "none");
 }
 
 // start today the input date today
 const dateInput = document.getElementById("Date-fin");
-const today = new Date().toISOString().split("T")[0];
-dateInput.setAttribute("min", today);
+const today = new Date().toISOString().split("T")[0]; // get the current date (day), and returns a date in string and split the string
+dateInput.setAttribute("min", today); // to block the date before today
 dateInput.addEventListener("keypress", function (e) {
-  //to block the enter user
-  e.preventDefault();
+  e.preventDefault(); //to block the enter user
 });
